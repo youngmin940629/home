f9363be5022c34f1da875d0f2efe3a92d26b7c31
youngmin <kgjdudals@naver.com>
2022-02-26 12:09:57 +0900

    Updates

---
+++ precache-manifest.bb2433770246bc033bd79472d06dd4d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312039db60eb189b08abc6fc20494d80",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "b3d1bac14b9693bf29cb",
+    "url": "/home/static/css/main.af6d9b4f.chunk.css"
+  },
+  {
+    "revision": "1a348f2b7ce3d35d490f",
+    "url": "/home/static/js/2.0af42362.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.0af42362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d1bac14b9693bf29cb",
+    "url": "/home/static/js/main.5f1c19a3.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "ccef1cb8d313c82ee915400c6c651bd2",
+    "url": "/home/static/media/profile.ccef1cb8.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.bb2433770246bc033bd79472d06dd4d2.js"
 );
 
 self.addEventListener('message', (event) => {

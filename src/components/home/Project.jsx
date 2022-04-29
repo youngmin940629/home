ed73070009283b4a9874b1eec7f0c0a28a6722a2
YoungMin <kgjdudals@naver.com>
2022-04-29 22:17:44 +0900

    modify

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -28,16 +28,25 @@ const Project = ({ heading, username, length, specfic }) => {
   const fetchRepos = useCallback(async () => {
     let repoList = [];
     try {
-      for (let repoName of specfic) {
-        const response = await axios.get(`${specficReposAPI}/${repoName}`);
-        repoList.push(response.data);
+      // getting all repos
+      const response = await axios.get(allReposAPI);
+      // slicing to the length
+      repoList = [...response.data.slice(0, length)];
+      // adding specified repos
+      try {
+        for (let repoName of specfic) {
+          const response = await axios.get(`${specficReposAPI}/${repoName}`);
+          repoList.push(response.data);
+        }
+      } catch (error) {
+        console.error(error.message);
       }
+      // setting projectArray
+      // TODO: remove the duplication.
+      setProjectsArray(repoList);
     } catch (error) {
-      console.error(error.message);
+      console.error(error.message)
     }
-    // setting projectArray
-    // TODO: remove the duplication.
-    setProjectsArray(repoList);
   }, [allReposAPI, length, specfic, specficReposAPI]);
 
   useEffect(() => {
